f6e5cbe3da2c44723df5f77a95f1321ccbfa719f
JonYDM <jonyocampo05@gmail.com>
2024-12-01 04:10:36 -0600

    A ver si quiere jalar la imagen

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   publicDir: './public',
   integrations: [tailwind()],
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
