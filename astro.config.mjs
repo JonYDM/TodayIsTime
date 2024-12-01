ca0dd940c00aead6acb45d3e24021481df69b47b
JonYDM <jonyocampo05@gmail.com>
2024-12-01 04:40:32 -0600

    VAMO A VER SI FUNCIONA CON EL VERCEL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import netlify from '@astrojs/netlify';
 
+import vercel from '@astrojs/vercel/serverless';
+
 export default defineConfig({
   publicDir: './public',
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify({
-    edgeMiddleware: true
-  }),
+  adapter: vercel(),
 });
